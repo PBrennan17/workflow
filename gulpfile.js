50a55bc9d5c70b77302f72d51857db7c736ec097
Paul Brennan <p.brennan@exactabacus.com>
2019-04-26 10:46:12 +0100

    installed git-coffeescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+let gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    gulp_coffee = require('gulp-coffee');
+
+let coffee_Scripts = ['components/coffee/*.coffee'];
+
+
 gulp.task('log', done => {
     gutil.log('Workflows are awesome');
+    done();
+});
+
+gulp.task('coffee', done =>{
+
+    gulp.src(coffee_Scripts)
+    .pipe(gulp_coffee({bare: true})
+        .on('error', gutil.log)
+    )
+    .pipe(gulp.dest('components/scripts'))
     done();
 });
