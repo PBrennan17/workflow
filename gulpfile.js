e67ffb8078da53e669d233899baf91ffdc039cae
Paul Brennan <p.brennan@exactabacus.com>
2019-04-26 12:21:02 +0100

    concatanated js

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,28 @@
 let gulp = require('gulp'),
     gutil = require('gulp-util'),
-    gulp_coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee');
+    concat= require('gulp-concat');
 
-let coffee_Scripts = ['components/coffee/*.coffee'];
-
-
-gulp.task('log', done => {
-    gutil.log('Workflows are awesome');
-    done();
-});
+let coffee_Sources = ['components/coffee/*.coffee'];
+let jsSources= ['components/scripts/pixgrid.js',
+                'components/scripts/rclick.js', 
+                'components/scripts/tagline.js', 
+                'components/scripts/template.js'];
 
 gulp.task('coffee', done =>{
 
-    gulp.src(coffee_Scripts)
-    .pipe(gulp_coffee({bare: true})
+    gulp.src(coffee_Sources)
+    .pipe(coffee({bare: true})
         .on('error', gutil.log)
     )
     .pipe(gulp.dest('components/scripts'))
+    done();
+});
+
+gulp.task('js', done =>{
+
+    gulp.src(jsSources)
+    .pipe(concat('scripts.js'))
+    .pipe(gulp.dest('builds/development/js'))
     done();
 });
