01fafa62d2eb7f4e145e80fe4b21e5399d73331a
Paul Brennan <p.brennan@exactabacus.com>
2019-04-26 17:08:24 +0100

    installed compass for Sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 let gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
-    concat= require('gulp-concat');
+    coffee = require('gulp-coffee'),
+    concat= require('gulp-concat'),
+    compass= require('gulp-compass');
 
+let sassSources = ['components/sass/style.scss'];
 let coffee_Sources = ['components/coffee/*.coffee'];
 let jsSources= ['components/scripts/pixgrid.js',
                 'components/scripts/rclick.js', 
                 'components/scripts/tagline.js', 
                 'components/scripts/template.js'];
+
+    
 
 gulp.task('coffee', done =>{
 
@@ -24,5 +28,18 @@ gulp.task('js', done =>{
     gulp.src(jsSources)
     .pipe(concat('scripts.js'))
     .pipe(gulp.dest('builds/development/js'))
+    done();
+});
+
+gulp.task('compass', done =>{
+
+    gulp.src(sassSources)
+    .pipe(compass({
+        sass: 'components/sass',
+        image: 'builds/development/images',
+        style: 'expanded'
+    }))
+        .on('error',gutil.log )
+    .pipe(gulp.dest('builds/development/css'))
     done();
 });
